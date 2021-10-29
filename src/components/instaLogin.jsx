e0927a9aab3abb6ad9d8e8d29f86e7d4cd45523e
dhanraj-web <dhanraj@aequalisys.com>
2021-10-29 15:38:01 +0530

    updated

--- src/components/instaLogin.jsx
+++ src/components/instaLogin.jsx
@@ -5,18 +5,25 @@ import InstagramLogin from "react-instagram-login";
 const InstaLogin = () => {
   const getUserAccessCode = async (res) => {
     console.log(res);
-    const formData = {
-      client_id: "605028720691089",
-      client_secret: "4936eebe886d1bde895304f850e0b344",
-      grant_type: "authorization_code",
-      redirect_uri: "https://getinstanft.herokuapp.com/",
-      code: res,
-    };
+    const formdata = new FormData();
+    formdata.append("client_id", "605028720691089");
+    formdata.append("client_secret", "4936eebe886d1bde895304f850e0b344");
+    formdata.append("grant_type", "authorization_code");
+    formdata.append("redirect_uri", "https://getinstanft.herokuapp.com/");
+    formdata.append("code", res);
+    // const formData = {
+    //   client_id: "605028720691089",
+    //   client_secret: "4936eebe886d1bde895304f850e0b344",
+    //   grant_type: "authorization_code",
+    //   redirect_uri: "https://getinstanft.herokuapp.com/",
+    //   code: res,
+    // };
+    console.log(formdata);
     try {
       await axios({
         method: "post",
         url: "https://api.instagram.com/oauth/access_token",
-        data: formData,
+        data: formdata,
         header: { "content-type": "multipart/form-data" },
       }).then((res) => console.log(res));
       // await axios.post(
