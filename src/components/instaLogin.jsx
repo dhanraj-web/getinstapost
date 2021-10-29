ed6620c83038fee44f9183de32eec789815bcc7b
dhanraj-web <dhanraj@aequalisys.com>
2021-10-29 13:01:08 +0530

    bug fix

--- src/components/instaLogin.jsx
+++ src/components/instaLogin.jsx
@@ -7,15 +7,15 @@ const InstaLogin = () => {
     console.log(response);
   };
 
-  const getUserAccessCode = (res) => {
+  const getUserAccessCode = async (res) => {
     console.log(res);
-    axios
+    await axios
       .post("https://api.instagram.com/oauth/access_token", {
         client_id: "605028720691089",
         client_secret: "4936eebe886d1bde895304f850e0b344",
         grant_type: "authorization_code",
         redirect_uri: "https://getinstanft.herokuapp.com/",
-        code: "AQAnLu3I2AvjXYzRbbnsohYJ_A1_kE5GJqbNeTW-ftRsLPOnQm_lyUt2QJyTYZ0u_zqSyu7oEco8TN1Rn-c65gKLnxw03_QtmAUqp65cnwPLgsIDRYyGJctGRvieB5jsizP7InXuZwPEXBZn4RmfQxgSwIinqdJrLieKpLDYIa7Y95uZKYBK-EtfpK-3Az9Kq8YJ8hdAH7g9EneWC4a40lVBywIOAfpn5FjTfZe0Jympog",
+        code: { res },
       })
       .then((response) => {
         console.log(response);
@@ -26,7 +26,7 @@ const InstaLogin = () => {
     <InstagramLogin
       clientId="605028720691089"
       buttonText="Login"
-      onSuccess={() => getUserAccessCode()}
+      onSuccess={(res) => getUserAccessCode(res)}
       onFailure={(res) => console.log(res)}
       scope={["user_profile", "user_media"]}
       redirectUri="https://getinstanft.herokuapp.com/"
